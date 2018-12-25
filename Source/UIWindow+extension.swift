a296321e383874068b9e48d0e4d24fdde6b47f03
Angelo Pino <pino.angelo@gmail.com>
2018-12-25 03:34:35 +0100

    add params for replace root controller in uiwindow extension

--- Source/UIWindow+extension.swift
+++ Source/UIWindow+extension.swift
@@ -9,43 +9,30 @@
 import UIKit
 
 public extension UIWindow {
-    public func replaceRootViewControllerWith(_ replacementController: UIViewController, animated: Bool, completion:(() -> Void)?) {
-        let snapshotImageView = UIImageView(image: self.snapshot())
-        self.addSubview(snapshotImageView)
-        if let presented = self.rootViewController?.presentedViewController {
-            presented.dismiss(animated: false, completion: { [weak self] in
-                guard let _self = self else {
-                    return
-                }
-                _self._replace(replacementController, snapshotImageView: snapshotImageView, animated: animated, completion: completion)
-            })
+    public func replaceRootViewControllerWith(_ replacementController: UIViewController, animated: Bool = true, duration: TimeInterval = 0.5, options: UIView.AnimationOptions = .transitionFlipFromRight, completion: (() -> Void)? = nil) {
+        
+        let replaceCallback: () -> Void = {
+            self.rootViewController?.removeFromParent()
+            self.rootViewController?.view.removeFromSuperview()
+            self.rootViewController = nil
+            self.rootViewController = replacementController
         }
-        else {
-            self._replace(replacementController, snapshotImageView: snapshotImageView, animated: animated, completion: completion)
-        }
-    }
-    
-    // MARK: private methods
-    private func _replace(_ replacementController: UIViewController, snapshotImageView: UIImageView, animated: Bool, completion: (() -> Void)?) {
-        self.rootViewController?.removeFromParentViewController()
-        self.rootViewController?.view.removeFromSuperview()
-        self.rootViewController = nil
-        self.rootViewController = replacementController
-        self.bringSubview(toFront: snapshotImageView)
         
         let completionCallback: (Bool) -> Void = { (Bool) in
-            snapshotImageView.removeFromSuperview()
             replacementController.modalPresentationCapturesStatusBarAppearance = true
             replacementController.setNeedsStatusBarAppearanceUpdate()
             completion?()
         }
         
         if animated {
-            UIView.animate(withDuration: 0.5, animations: { () -> Void in
-                snapshotImageView.alpha = 0
+            UIView.transition(with: self, duration: duration, options: options, animations: {
+                let oldState: Bool = UIView.areAnimationsEnabled
+                UIView.setAnimationsEnabled(false)
+                replaceCallback()
+                UIView.setAnimationsEnabled(oldState)
             }, completion: completionCallback)
-        }
-        else {
+        } else {
+            replaceCallback()
             completionCallback(true)
         }
     }
