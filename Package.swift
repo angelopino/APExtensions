32e95a7e84f70842bb97c86a770b7e7ad6bc0ab9
Angelo Pino <pino.angelo@gmail.com>
2020-04-15 02:59:47 +0200

    upgrade version to ios version 13

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "APJExtensions",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
